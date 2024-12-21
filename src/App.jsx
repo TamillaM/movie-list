2c0dc24cc197bff53366d11d765c49b3556ef7f5
Tammilla <tamilla19820204@gmail.com>
2024-12-21 19:10:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "../src/pages/Home";
+import './css/App.css';
+import Favorites from "./pages/Favorites";
+import {Routes,Route} from "react-router-dom";
+import { MovieProvider } from "./contex/MovieContex";
+import NavBar from "./components/NavBar"
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+    <main className="main-content">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+      </Routes>
+
+    </main>
+    </MovieProvider>
+        
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "b2194c24d518c30338f1a6f0af62cb24"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+    )}`);
+    const data = await response.json()
+    return data.results
+};
